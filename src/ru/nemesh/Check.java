7fa32525061781ad459ef5adf493f03850f5e7b8
Konstantin <nemeshkv@gmail.com>
2023-01-23 00:17:09 +0500

    пример api

--- src/ru/nemesh/Check.java
+++ src/ru/nemesh/Check.java
@@ -1,8 +1,39 @@
 package ru.nemesh;
 
 public class Check {
-    public static String getHownum(String input){
+    public static int getHownum(String input) throws Exception {
+        boolean resultNumeric = Numeric(input);
 
-        return null;
+        if (resultNumeric == (true)) {
+            int b = Integer.parseInt(input);
+            boolean c = NegativeNumber(Integer.parseInt(input));
+            if (c == true) {
+                throw new NegativeException("Число должно быть положительным!!!");
+            } else
+                return b;
+//        } else {
+//            if (resultNumeric == (false)) ;
+//            return checkRom(a);
+        }
+        return 0;
+    }
+
+    public static boolean Numeric(String a) {
+        try {
+            Double.parseDouble(a);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+
+    private static boolean NegativeNumber(int a) {
+
+        if (a < 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/ru/nemesh/Main.java
+++ src/ru/nemesh/Main.java
@@ -1,23 +1,30 @@
 package ru.nemesh;
 
+import javax.swing.*;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws Exception{
+        System.out.println("Введите ИНН");
         Scanner scanner = new Scanner(System.in);
         String input = null;   
         String keyapi = "125633555856622";
         String result = null;
 
+        JButton button = new JButton("Найти");
+        ActionListener actionListener = new ToActionListener();
+        button.addActionListener(actionListener);
         input = scanner.nextLine();
 //        try {
-            input = Check.getHownum(input);
+            input = String.valueOf(Check.getHownum(input));
 //
 //        } catch (NumException e) {
 //            System.out.println(e.getMessage());
            // continue;
         //}
       result = String.valueOf((Senda.checkOrg(input,keyapi)));
+        System.out.println("Информация по организации");
     }
 }
