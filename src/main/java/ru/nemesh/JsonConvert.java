7a559f0ded3f0d7476629f4a36594b071db7215b
Konstantin <nemeshkv@gmail.com>
2023-02-24 20:50:26 +0500

    12 Check+Stream+Mainservlet test24022023

--- src/main/java/ru/nemesh/JsonConvert.java
+++ src/main/java/ru/nemesh/JsonConvert.java
@@ -1,16 +1,18 @@
-package ru.nemesh;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.util.stream.Stream;
-
-
-public class JsonConvert {
-    public static String convertJson(Stream a) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            mapper.writeValueAsString(a);
+//package ru.nemesh;
+//
+////import com.fasterxml.jackson.core.JsonProcessingException;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import ru.nemesh.exception.JsonProcessingException;
+//
+//import java.util.stream.Stream;
+//
+//
+//public class JsonConvert {
+//    public static String convertJson(Stream a) throws JsonProcessingException {
+//        ObjectMapper mapper = new ObjectMapper();
+//        try {
+//            mapper.writeValueAsString(a);
             //читаем а
             //сравниваем а и поля в классе ResultInn
             //если тру то это поле отправяем сюда:
@@ -19,16 +21,18 @@ public class JsonConvert {
             //если тру то это поле отправяем сюда:
             //ResultInn.setАдресПолн();
 
-            String result;
-     //     return result = ResultInn.setНАИМПОЛНЮЛ() || ResultInn.setАдресПолн();
-            //          System.out.println("ResultingJSONstring = " + json);    //
-            //System.out.println(json);                                         //
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-}
+//            String result;
+//     //     return result = ResultInn.setНАИМПОЛНЮЛ() || ResultInn.setАдресПолн();
+//            //          System.out.println("ResultingJSONstring = " + json);    //
+//            //System.out.println(json);                                         //
+////        } catch (JsonProcessingException | com.fasterxml.jackson.core.JsonProcessingException e) {
+////            e.printStackTrace();
+//        } catch (com.fasterxml.jackson.core.JsonProcessingException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//}
 
 //    public static String convertString(String jsonString) throws JsonProcessingException {
 //        ObjectMapper mapper = new ObjectMapper();
